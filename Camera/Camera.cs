51dfe9713e78d4693e17596d3e560a870339768b
Kimserey Lam <kimserey.lam@gmail.com>
2016-10-01 23:27:43 +0100

    image is empty

--- Camera/Camera.cs
+++ Camera/Camera.cs
@@ -32,10 +32,14 @@ namespace Camera
 					Text = "Take photo"
 				};
 
+			var image =
+				new Image { HeightRequest = 100, BackgroundColor = Color.Aqua };
+
 			button.Clicked += async (sender, e) =>
 			{
 				var result = await DependencyService.Get<IImageCapture>().Capture();
 				label.Text = result.Path;
+				image.Source = ImageSource.FromFile(result.Path);
 			};
 
 			var content = new ContentPage
@@ -46,6 +50,7 @@ namespace Camera
 					VerticalOptions = LayoutOptions.Center,
 					Children = {
 						label,
+						image,
 						button
 					}
 				}
